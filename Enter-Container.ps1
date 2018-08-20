efe5d64b50ce43db60c8a8d6285e75cde71ed380
Micah R Ledbetter <me@micahrl.com>
2018-08-20 01:14:20 -0500

    Fix typos/corner cases in Enter-Container.ps1

--- Enter-Container.ps1
+++ Enter-Container.ps1
@@ -9,8 +9,10 @@
     $BackupSchedule = $env:BACKUP_SCHEDULE
 )
 
+# $ErrorActionPreference = "Stop"
+
 $awsConfig = Import-PowerShellDataFile -Path $AwsConfigPath
-Set-AWSCredential -AccessKey $awsConfig.AwsAccessKey -SecretKey $awsConfig.AwsSecretKey -Region AwsRegion -StoreAs 'default'
+Set-AWSCredential -AccessKey $awsConfig.AwsAccessKey -SecretKey $awsConfig.AwsSecretKey -StoreAs 'default'
 
 $gpgKeyRegex = "^gpg\: key (?<keyid>[0-9A-F]{8})\:"
 $slsMatch = gpg2 --import "$GpgRecipientKeyPath" 2>&1 |
@@ -19,7 +21,10 @@ $recipientKeyId = $slsMatch.Matches.Groups |
     Where-Object -Property Name -EQ 'keyid' |
     Select-Object -ExpandProperty Value
 
-"${BackupSchedule} pwsh -NoLogo -NonInteractive -NoProfile -File /usr/local/bin/Backup-SwarmVolume.ps1 -ParameterFile '$ParameterPath'" | crontab -e
+"${BackupSchedule} pwsh -NoLogo -NonInteractive -NoProfile -File /usr/local/bin/Backup-SwarmVolume.ps1 -ParameterFile '$ParameterPath'" | crontab -
 cron
 
-Get-Content -Tail -Path "/var/log/bsv.log"
+if (-not (Test-Path -Path /var/log/bsv.log)) {
+    New-Item -ItemType File -Path /var/log/bsv.log
+}
+Get-Content -Wait -Path /var/log/bsv.log
